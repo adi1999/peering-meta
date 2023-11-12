e49ca261e440688d1cb980f78fe7fbde07c9b2b4
Harsh Lohana <harshlohana@Harshs-MacBook-Pro.local>
2023-11-13 00:50:49 +0530

    table

--- ui/src/table.js
+++ ui/src/table.js
@@ -1,33 +1,5 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
 
 export default function CustomizedTables(data) {
   const [rows, setRows] = useState([{}]);
@@ -39,35 +11,47 @@ export default function CustomizedTables(data) {
   }, [data]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Willingness Score</StyledTableCell>
-            <StyledTableCell align="right">Affinity Score</StyledTableCell>
-            <StyledTableCell align="right">Rank</StyledTableCell>
-            <StyledTableCell align="right">Location</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, index) => (
-            <StyledTableRow key={index}>
-              <StyledTableCell component="th" scope="row">
-                {row.Decision_willingness_score}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                {" "}
-                {row.Affinity_Score}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.Order}</StyledTableCell>
-              <StyledTableCell align="right">
-                {" "}
-                {row.peering_location}{" "}
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <table>
+      <thead>
+        <tr>
+          <th>Column1</th>
+          <th>Column2</th>
+          <th>Column3</th>
+          <th>Column4</th>
+          <th>Column5</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+        </tr>
+        <tr>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+        </tr>
+        <tr>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+        </tr>
+        <tr>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+          <td>Demo value</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
